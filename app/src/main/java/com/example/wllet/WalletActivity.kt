729104b4eee21cae7bb10def1bd2e3823e16ca6c
a4r7gc4ttfwsqcey <admin@linuxmasterrace.com>
2021-04-13 09:54:10 +0300

    Add card number validation and type detection functionality

--- app/src/main/java/com/example/wllet/WalletActivity.kt
+++ app/src/main/java/com/example/wllet/WalletActivity.kt
@@ -1,11 +1,19 @@
 package com.example.wllet
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class WalletActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_wallet)
+    }
+    fun addcard(view: View) {
+        view.setOnClickListener {
+            val intent = Intent(this, AddCardActivity::class.java)
+            view.context.startActivity(intent)
+        }
     }
 }
