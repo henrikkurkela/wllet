876021b7181181e15cd07bb3434527e474b0512e
Joni Arstio <joni.arstio@gmail.com>
2021-04-20 15:57:35 +0300

    firebase integration

--- app/src/main/java/com/example/wllet/DealsActivity.kt
+++ app/src/main/java/com/example/wllet/DealsActivity.kt
@@ -2,26 +2,50 @@ package com.example.wllet
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.google.firebase.database.*
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
 import kotlinx.android.synthetic.main.activity_deals.*
 
 class DealsActivity : AppCompatActivity() {
+
+    private lateinit var database: DatabaseReference
+    private lateinit var deals: ArrayList<DealsModel>
+    private lateinit var rcDealsList: RecyclerView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_deals)
 
-        val arrayList = ArrayList<DealsModel>()
+        deals = arrayListOf<DealsModel>()
+        database = Firebase.database.reference
+        rcDealsList = findViewById(R.id.recyclerView)
 
-        arrayList.add(DealsModel("Netflix", "7.99€", R.drawable.placeholder))
-        arrayList.add(DealsModel("Spotify", "10.99€", R.drawable.placeholder))
-        arrayList.add(DealsModel("Disney+", "8.99€", R.drawable.placeholder))
-        arrayList.add(DealsModel("HBO", "10.95€", R.drawable.placeholder))
+        val dealsListener = object: ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+                val dealsFromDatabase = snapshot.child("deals")
+                deals.clear()
 
-        val dealsAdapter = DealsAdapter(arrayList, this)
+                for (snapshot in dealsFromDatabase.children) {
+                    val deep = snapshot.value as HashMap<String, String>
+                    val deal: DealsModel = DealsModel.from(deep)
+                    deals.add(deal)
+                }
+                rcDealsList.adapter?.notifyDataSetChanged()
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                Log.d("wllet", error.toString())
+            }
+        }
+        database.addValueEventListener(dealsListener)
+
+        val dealsAdapter = DealsAdapter(deals, this)
 
         recyclerView.layoutManager = LinearLayoutManager(this)
         recyclerView.adapter = dealsAdapter
     }
 }
-
--- app/src/main/java/com/example/wllet/DealsModel.kt
+++ app/src/main/java/com/example/wllet/DealsModel.kt
@@ -1,4 +1,13 @@
 package com.example.wllet
 
 class DealsModel(val title: String, val price: String, val image: Int) {
+    companion object {
+        fun from(map: HashMap<String, String>) = object {
+            val title by map
+            val price by map
+            val image by map
+
+            val data = DealsModel(title, price, image = R.drawable.placeholder)
+        }.data
+    }
 }
